53d183f4019865461a438c80d3849206603b1c76
Kol <31559413+SmirnovKol@users.noreply.github.com>
2018-09-30 18:58:37 +0800

    Add files via upload

---
+++ LinkedList.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct LNode {
+    ElemType data;
+    struct LNode *next;
+} LNode, *LinkList;
+
+Status ListCreate_L(LinkList *L, int n)
+{
+    int i;
+    LNode *p;
+    ElemType e;
+
+    *L = (LinkList) malloc(sizeof(LNode));
+    if (*L == NULL)
+        return OVERFLOW;
+    (*L)->next = NULL;
+
+    for (i = n; i > 0; --i) {
+        p = (LNode *) malloc(sizeof(LNode));
+        if (p == NULL)
+            return OVERFLOW;
+        scanf("%d", &e);
+        p->data = e; p->next = (*L)->next;
+        (*L)->next = p;
+    }
+
+    return OK;
+}
+
+Status GetElem_L(LinkList L, int i, ElemType *e)
+{
+    // when the i-th element exits, assign its value to e and
+    // return OK , otherwise return ERROR
+    int count = 1;
+    LNode *p = L->next;
+
+    while (p && count < i) {
+        p = p->next;
+        ++count;
+    }
+    if (!p || count > i)
+        return ERROR;
+    *e = p->data;
+    return OK;
+}
+
+// no need to use "LinkList *L", L is itself a pointer
+Status ListInsert_L(LinkList L, int i, ElemType e)
+{
+    // insert e before the i-th element
+    LNode *p = L;
+    LNode *s = NULL;
+    int j = 0;
+
+    while (p && j < i-1) {
+        p = p->next;
+        ++j;
+    }
+    if (!p || j > i-1)
+        return ERROR;
+    s = (LNode *) malloc(sizeof(LNode));
+    if (!s)
+        return OVERFLOW;
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return OK;
+}
+
+Status ListDelete_L(LinkList L, int i, ElemType *e)
+{
+    LNode *p = L;
+    int j = 0;
+    LNode *q = NULL;
+
+    while (p->next && j < i-1) {
+        p = p->next;
+        ++j;
+    }
+    if (!(p->next) || j > i-1)
+        return ERROR;
+    q = p->next;    p->next = q->next;
+    *e = q->data;   free(q);     // Caution!!!
+    return OK;
+}
+
+void ListPrint_L(LinkList L)
+{
+    LNode *p = L->next;
+
+    if (p == NULL)
+        printf("empty list!!!\n");
+    else {
+        do {
+            printf("%d -> ", p->data);
+            p = p->next;
+        } while (p != NULL);
+        printf("NULL\n");
+    }
+}
+
+void MergeList_L(LinkList *La, LinkList *Lb, LinkList *Lc)
+{
+    // merge two non-decreasing lists La and Lb to get
+    // a non-decreasing list Lc
+    LNode *pa, *pb, *pc;
+    pa = (*La)->next; pb = (*Lb)->next;
+    pc = *Lc = *La;
+
+    while (pa && pb) {
+        if (pa->data <= pb->data) {
+            pc->next = pa;
+            pc = pa;
+            pa = pa->next;
+        } else {
+            pc->next = pb;
+            pc = pb;
+            pb = pb->next;
+        }
+    }
+    pc->next = pa ? pa : pb;
+    free(*Lb);
+}
+
---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct LNode {
+    ElemType data;
+    struct LNode *next;
+} LNode, *LinkList;     // LinkList is the pointer to the head node
+
+Status ListCreate_L(LinkList *L, int n);
+Status ListInsert_L(LinkList L, int i, ElemType e);
+Status ListDelete_L(LinkList L, int i, ElemType *e);
+void ListPrint_L(LinkList L);
+void MergeList_L(LinkList *La, LinkList *Lb, LinkList *Lc);
+
+int main()
+{
+    LinkList L, L2, Lc;
+    ElemType e = 11;
+    ListCreate_L(&L, 5);
+    ListPrint_L(L);
+
+    ListInsert_L(L, 1, e);
+    e = 12;
+    ListInsert_L(L, 2, e);
+    e = 13;
+    ListInsert_L(L, 3, e);
+    ListPrint_L(L);
+
+    ListDelete_L(L, 2, &e);
+    ListPrint_L(L);
+    printf("\n the second element = %d\n", e);
+    ListDelete_L(L, 1, &e);
+    ListDelete_L(L, 1, &e);
+    ListPrint_L(L);
+
+    printf("\nCreate the second list:\n");
+    ListCreate_L(&L2, 5);
+    MergeList_L(&L, &L2, &Lc);
+    ListPrint_L(Lc);
+    ListPrint_L(L);
+
+    return 0;
+}
